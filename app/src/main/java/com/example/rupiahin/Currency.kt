866cb8af2d8576f3733d3a0bad26e732dc1fe9ba
Dwicandra <dwicandra.ekastrya@gmail.com>
2021-03-14 08:07:52 +0700

    use Gson to parse Json value from REST API

--- app/src/main/java/com/example/rupiahin/Currency.kt
+++ app/src/main/java/com/example/rupiahin/Currency.kt
@@ -1,26 +1,27 @@
 package com.example.rupiahin
 
-class Currency(val amount: Int) {
-    fun getFormattedValue(): String {
-        var result: String = ""
-        var remainder: Int
-        var dividen = amount
-
-        do {
-            remainder = dividen % 1000
-            result = "${remainder}" + result
-            dividen = dividen / 1000
-
-            if(dividen > 0) {
-                if(remainder < 100)
-                    result = "0" + result
-                if(remainder < 10)
-                    result = "0" + result
-
-                result = "." + result
-            }
-        } while (dividen > 0)
-
-        return result
-    }
-}
+data class Currency(val idr_value: String)
+//{
+//    fun getFormattedValue(): String {
+//        var result: String = ""
+//        var remainder: Int
+//        var dividen = idr_value
+//
+//        do {
+//            remainder = dividen % 1000
+//            result = "${remainder}" + result
+//            dividen = dividen / 1000
+//
+//            if(dividen > 0) {
+//                if(remainder < 100)
+//                    result = "0" + result
+//                if(remainder < 10)
+//                    result = "0" + result
+//
+//                result = "." + result
+//            }
+//        } while (dividen > 0)
+//
+//        return result
+//    }
+//}
--- app/src/main/java/com/example/rupiahin/MainActivity.kt
+++ app/src/main/java/com/example/rupiahin/MainActivity.kt
@@ -7,29 +7,58 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.google.gson.Gson
 import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val btnHitung: Button = findViewById(R.id.btn_hitung)
-        val edtAmout: EditText = findViewById(R.id.edt_usd_value)
+        val edtAmout:EditText = findViewById(R.id.edt_usd_value)
         val tvKeterangan: TextView = findViewById(R.id.tv_keterangan)
         val tvLabelHasil: TextView = findViewById(R.id.tv_label_hasil)
         val tvHasil: TextView = findViewById(R.id.tv_hasil)
         val rate: Int = 14500
 
         btnHitung.setOnClickListener {
-            val toast = Toast.makeText(this, "Button clicked", Toast.LENGTH_SHORT)
-            var hasil: Int = edtAmout.text.toString().toInt() * rate
-            toast.show()
-            tvKeterangan.visibility = View.INVISIBLE
-            tvLabelHasil.visibility = View.VISIBLE
-            tvHasil.visibility = View.VISIBLE
-            val money = Currency(hasil)
-            tvHasil.setText("Rp ${money.getFormattedValue()}")
+            var hasil: String = edtAmout.text.toString()
+            val queue = Volley.newRequestQueue(this)
+            val url = "https://travelspot.xyz/api/moneyapp/usd-to-idr?amount=%s".format(hasil)
+
+            // Request a string response from the provided URL.
+            val stringRequest = StringRequest(Request.Method.GET, url,
+                    Response.Listener<String> { response ->
+                        // Display the first 500 characters of the response string.
+                        tvKeterangan.visibility = View.INVISIBLE
+                        tvLabelHasil.visibility = View.VISIBLE
+                        tvHasil.visibility = View.VISIBLE
+                        val money = Gson().fromJson(response, Currency::class.java)
+                        tvHasil.setText("Rp ${money.idr_value}")
+                    },
+                    Response.ErrorListener {
+                        var hasil: Int = edtAmout.text.toString().toInt() * rate
+                        val toast = Toast.makeText(this, "Network error, result may not be accurate", Toast.LENGTH_LONG)
+                        toast.show()
+                        tvKeterangan.visibility = View.INVISIBLE
+                        tvLabelHasil.visibility = View.VISIBLE
+                        tvHasil.visibility = View.VISIBLE
+                        val money = Currency(hasil.toString())
+                        tvHasil.setText("Rp ${money.idr_value}")
+//                        tvHasil.setText("Rp ${money.getFormattedValue()}")
+                    })
+
+            // Add the request to the RequestQueue.
+            queue.add(stringRequest)
         }
     }
+
 }
